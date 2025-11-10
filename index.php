22fbe0e71696f417d35ee19e0e322a3335c762d1
if24nawafikhwan <if24.nawafikhwan@mhs.ubpkarawang.ac.id>
2025-11-10 11:59:34 +0700

    pertemuan4

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once('./LuasLingkaran.php'); // memanggil file class
-use App\Math\LuasLingkaran;
-
-$lingkaran = new LuasLingkaran(10);
-$lingkaran->tampil('roda');
--- luaslingkaran.php
+++ luaslingkaran.php
@@ -1,20 +1,23 @@
 <?php
 
-namespace App\Math;
-
 class LuasLingkaran {
-    public const PHI = 3.14;
-    protected int $jari;
 
-    public function __construct($isiJari = 1) {
-        $this->jari = $isiJari;
+    public const phi = 3.14;
+    public int $jari;
+
+    public function tampil($nama = 'ban') {
+        $rumus = LuasLingkaran::phi * ($this->jari * $this->jari);
+        echo "Lingkaran {$nama} hasilnya adalah: {$rumus}";
     }
 
-    public function hitungLuas(): float {
-        return self::PHI * ($this->jari ** 2);
-    }
-
-    public function tampil($nama = 'ban'): void {
-        echo "Luas lingkaran {$nama} = " . $this->hitungLuas();
+    public static function testing() {
+        echo "<br/>";
+        echo "ini dari static";
     }
 }
+
+$lingkaran = new LuasLingkaran();
+$lingkaran->jari = 12;
+$lingkaran->tampil('roda'); //panggil method tampil
+
+LuasLingkaran::testing(); //panggil static method
